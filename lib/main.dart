f2e002fe9f05bd3ba6d0485e5527dabc7d017efd
JarlissonTomaz <tomazj995@gmail.com>
2024-12-10 16:39:11 -0300

    build: share img

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
 
 void main() {
@@ -31,19 +35,50 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final Image imageTest = Image.asset('assets/images/images.png');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Share'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            shareText();
-          },
-          child: const Text('Share'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                shareText();
+              },
+              child: const Text('Share Text'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                _shareImageFromAssets();
+              },
+              child: const Text('Share Img'),
+            ),
+          ],
         ),
       ),
     );
+  }
+}
+
+Future<void> _shareImageFromAssets() async {
+  try {
+    // Carrega a imagem dos assets
+    final byteData = await rootBundle.load('assets/images/images.png');
+
+    // Salva a imagem no diretório temporário
+    final tempDir = await getTemporaryDirectory();
+    final filePath = '${tempDir.path}/images.png';
+    final file = File(filePath);
+    await file.writeAsBytes(byteData.buffer.asUint8List());
+
+    // Compartilha a imagem
+    final xFile = XFile(filePath);
+    await Share.shareXFiles([xFile], text: 'Confira esta imagem!');
+  } catch (e) {
+    print("Erro ao compartilhar a imagem: $e");
   }
 }
 
